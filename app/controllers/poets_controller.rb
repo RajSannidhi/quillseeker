f5b1cb268ded0aa449538e197c6644687082c265
RajSannidhi <rajesh.v.sannidhi@gmail.com>
2017-07-07 12:27:02 -0400

    Connected links in navbar

--- app/controllers/poets_controller.rb
+++ app/controllers/poets_controller.rb
@@ -4,8 +4,19 @@ class PoetsController < ApplicationController
     @poets = Poet.all
   end
 
-def new
-  @poet = Poet.new
-end
+  def new
+    @poet = Poet.new
+  end
+
+  def create
+    Poet.create(poet_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def poet_params
+     params.require(:poet).permit(:name, :description, :dates)
+  end
 
 end
